a767a4e5ed3b0abf9dbf4fa115edca288507796e
vinnikoff <winr7post@gmail.com>
2016-12-10 14:15:05 +0200

    project6-1-3

--- ArraysUtils.java
+++ ArraysUtils.java
@@ -4,103 +4,63 @@
 import java.util.*;
 final public class ArraysUtils {
 
-    public static void main(String[] args) {
-        int arr1[] = {7, 7, -5, 0, -33, -5, -4, 1, -33, 2};
-        int arrravno = 0;
-        for (int z = 0; z < arr1.length - 1; z++) {
-            if (arr1[z] != arr1[z + 1])
-                arrravno = 1;
-        }
 
-        System.out.println("Исходный массив:");
-        System.out.println(Arrays.toString(arr1));
-        sum(arr1);
-        min(arr1, arrravno);
-        max(arr1, arrravno);
-        maxPositive(arr1, arrravno);
-        multiplication(arr1);
-        modulus(arr1);
-        reverse(arr1);
-        findEvenElements(arr1);
-        secondLagest(arr1, arrravno);
+    public static final void min(int[] arr) {
 
-
-    }
-
-    public static void min(int[] arr, int arrravno) {
-        if (arrravno == 0)
-            System.out.println("min: все эл. массива равны");
-        else {
             int min = arr[0];
             for (int i = 1; i <= arr.length - 1; ++i) {
                 if (arr[i] < min) min = arr[i];
             }
-            System.out.println("min:" + min);
         }
-    }
 
-    public static void max(int[] arr, int arrravno) {
-        if (arrravno == 0)
-            System.out.println("max: все эл. массива равны");
-        else {
+
+    public static final void max(int[] arr) {
+
             int max = arr[0];
             for (int i = 1; i <= arr.length - 1; ++i) {
                 if (arr[i] > max) max = arr[i];
             }
 
-            System.out.println("max:" + max);
         }
-    }
 
-    public static void sum(int[] arr) {
+
+    public static final void sum(int[] arr) {
         int sum1 = 0;
         for (int i = 0; i <= arr.length - 1; i++) {
 
             sum1 = sum1 + arr[i];
         }
 
-        System.out.println("сумма элементов:" + sum1);
-
     }
 
-    public static void multiplication(int[] arr) {
+    public static final void multiplication(int[] arr) {
         int mult = 1;
         for (int i = 0; i <= arr.length - 1; i++) {
             mult = mult * arr[i];
         }
-        System.out.println("multiplication:" + mult);
+
     }
 
 
-    public static void maxPositive(int[] arr, int arrravno) {
-        if (arrravno == 0)
-            System.out.println("maxPositive: все эл. массива равны");
-        else {
-            int max = arr[0];
-            for (int i = 1; i <= arr.length - 1; ++i) {
-                if (arr[i] > max) max = arr[i];
-            }
-            if (max > 0) {
-                System.out.println("maxPositive:" + max);
-            } else System.out.println("Положительные элементы отсутствуют");
+    public static final void maxPositive(int[] arr) {
+
+        int max = arr[0];
+        for (int i = 1; i <= arr.length - 1; ++i) {
+            if (arr[i] > max) max = arr[i];
         }
+        if (max <= 0) max = 0;
     }
 
-    public static void modulus(int[] arr) {
+
+    public static final void modulus(int[] arr) {
         int first = arr[0];
         int last = arr[arr.length - 1];
-        if (last != 0) {
-
-            int mod = first % last;
-            System.out.println("modulus первого и последн. эл.:" + mod);
-        } else System.out.println("modulus: Последний эл. равен нулю");
+        int mod = first % last;
     }
 
 
-    public static void secondLagest(int[] arr, int arrravno) {
-        if (arrravno == 0)
-            System.out.println("secondLagest: все эл. массива равны");
-        else {
+    public static final void secondLagest(int[] arr) {
+
             for (int i = arr.length - 1; i > 0; i--) {
                 for (int j = 0; j < i; j++) {
 
@@ -112,18 +72,15 @@ final public class ArraysUtils {
                 }
             }
 
-
             for (int z = arr.length - 1; z > 0; z--) {
                 if (arr[z] != arr[z - 1]) {
-                    System.out.println("secondLagest:" + arr[z - 1]);
+
                     break;
                 }
             }
-        }
-
     }
 
-    public static int[] reverse(int[] arr2) {
+    public static final int[] reverse(int[] arr2) {
 
         int tmp;
         for (int i = 0; i < arr2.length / 2; i++) {
@@ -131,11 +88,11 @@ final public class ArraysUtils {
             arr2[arr2.length - 1 - i] = arr2[i];
             arr2[i] = tmp;
         }
-        System.out.println("reverse:" + Arrays.toString(arr2));
+
         return arr2;
     }
 
-    public static int[] findEvenElements(int[] arr2) {
+    public static final int[] findEvenElements(int[] arr2) {
         boolean mask[] = new boolean[arr2.length];
         int removeCount = 0;
 
@@ -153,7 +110,7 @@ final public class ArraysUtils {
                 result[j++] = arr2[i];
             }
         }
-        System.out.println("findEvenElements: " + Arrays.toString(result));
+
         return result;
     }
 
---
+++ UserUtils.java
@@ -0,0 +1,21 @@
+/**
+ * Created by VINNI on 10.12.16.
+ */
+public class UserUtils {
+
+    public static void main(String[] args) {
+        int arr1[] = {7, 7, -5, 0, -33, -5, -4, 1, -33, 2};
+
+        ArraysUtils.sum(arr1);
+        ArraysUtils.min(arr1);
+        ArraysUtils.max(arr1);
+        ArraysUtils.maxPositive(arr1);
+        ArraysUtils.multiplication(arr1);
+        ArraysUtils.modulus(arr1);
+        ArraysUtils.reverse(arr1);
+        ArraysUtils.findEvenElements(arr1);
+        ArraysUtils.secondLagest(arr1);
+
+    }
+}
+
